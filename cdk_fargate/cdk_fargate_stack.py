1c86be5ceb13082c1979f453aa4faf043bcfd4cd
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2021-05-09 18:25:20 -0400

    Added sample tasks

--- cdk_fargate/cdk_fargate_stack.py
+++ cdk_fargate/cdk_fargate_stack.py
@@ -1,15 +1,40 @@
 from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import aws_ecs as ecs, aws_ec2 as ec2
 
 
 class CdkFargateStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, vpc_id: ec2.Vpc.vpc_id, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        _vpc = ec2.Vpc.from_lookup(
+            self,
+            "vpc",
+            vpc_id=vpc_id
+        )
+
+        _cluster = ecs.Cluster(
+            self,
+            "ecscluster",
+            vpc=_vpc
+        )
+
+        _task_def = ecs.FargateTaskDefinition(
+            self,
+            "fargatetask",
+            memory_limit_mib=512,
+            cpu=256
+        )
+
+        _task_def.add_container(
+            "taskcontainer",
+            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")
+        )
+
+        ecs.FargateService(
+            self,
+            "fargateservuce",
+            cluster=_cluster,
+            task_definition=_task_def,
+            desired_count=5
+        )
